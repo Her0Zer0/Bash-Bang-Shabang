14a75308baf56545ddaf26a10a04f856886be13c
Robert Smith <resmith@pop-os.localdomain>
2020-09-20 11:03:13 -0500

    Adding first commit

---
+++ linux_command_instruction/zenity/adduser.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+    # zenity --info --text "Hi!"
+    # sleep 0.5
+    # zenity --question --text="Would you like ot participate in a little form?"
+    # sleep 1
+    zenity --width=622 --height=400 --info --text="Getting important run files"
